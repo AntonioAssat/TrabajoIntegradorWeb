d582fc9fd60ed5c86424cd50acd245de0d5e3bc1
AntonioAssat <turcotomas280302@gmail.com>
2025-10-15 22:39:50 -0300

    carruseljs, indexcss,carruselcss agregados

---
+++ scripts/carrusel.js
@@ -0,0 +1,32 @@
+const $ = (selector) => document.querySelector(selector);
+
+const $btn_back = $('#back');
+const $btn_next = $('#next');
+const $img = $('img');
+
+let count = 0;
+const images = [
+  'cratina',
+  'hydromax',
+  'protein-foods',
+  'protein',
+  'vitamina',
+];
+
+$img.src = `./public/image/${images[count]}.webp`;
+$img.alt = `creatina`;
+
+$btn_back.addEventListener('click', () => {
+  // operador ternario '?'
+  count = (count === 0) ? 4 : count - 1; 
+
+  $img.src = `./public/image/${images[count]}.webp`;
+  $img.alt = images[count];
+});
+
+$btn_next.addEventListener('click', () => {
+  count = (count === 4) ? 0 : count + 1;
+
+  $img.src = `./public/image/${images[count]}.webp`;
+  $img.alt = images[count];
+});
