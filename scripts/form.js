cde4628482b10518457558cef9cba72ac87c15b2
Esteban1609 <Estebanjose1609@gmail.com>
2025-10-16 19:24:04 -0300

    agregue register.html, form.js, y registrar.css

---
+++ scripts/form.js
@@ -0,0 +1,39 @@
+import Usuario from './Usuario.js'
+
+const $ = (selector) => document.querySelector(selector);
+
+const $form = $('form');
+const $resultado = $('.resultado');
+
+const $nombre = $('.nombre');
+const $apellido = $('.apellido');
+const $mail = $('.mail');
+const $contacto = $('.contacto');
+
+const $div_error = $('.error');
+const $span_error = $div_error.querySelector('span');
+
+$form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  const nombre = $form.querySelector('#nombre').value;
+  const apellido = $form.querySelector('#apellido').value;
+  const mail = $form.querySelector('#mail').value;
+  const contacto = $form.querySelector('#contacto').value;
+
+  try {
+    const usuario = new Usuario(nombre, apellido, mail, contacto);
+
+    $nombre.textContent = usuario.nombre;
+    $apellido.textContent = usuario.apellido;
+    $mail.textContent = usuario.mail;
+    $contacto.textContent = usuario.contacto;
+
+    $resultado.style.display = 'block';
+    $div_error.style.display = 'none';
+    $form.style.display = 'none';
+  } catch (err) {
+    $div_error.style.display = 'flex';
+    $span_error.textContent = err.message;
+  }
+});
